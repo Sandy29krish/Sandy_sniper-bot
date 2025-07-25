5d1079c26486be302c7f830a61b5e6ab223564df
Sandy <sandyk29krish@gmail.com>
2025-07-25 21:15:34 +0530

    Add files via upload

---
+++ utils/swing_config.py
@@ -0,0 +1,34 @@
+SWING_CONFIG = {
+    'NIFTY': {
+        'instrument_token': 256265,
+        'exchange': 'NSE',
+        'lot_size': 75,
+        'cpr_level': 17500,
+        'trend': 'uptrend',
+        'full_name': 'NIFTY 50 Index'
+    },
+    'BANKNIFTY': {
+        'instrument_token': 260105,
+        'exchange': 'NSE',
+        'lot_size': 30,
+        'cpr_level': 42000,
+        'trend': 'uptrend',
+        'full_name': 'Bank Nifty'
+    },
+    'SENSEX': {
+        'instrument_token': 11910258,
+        'exchange': 'BSE',
+        'lot_size': 10,
+        'cpr_level': 58000,
+        'trend': 'uptrend',
+        'full_name': 'SENSEX'
+    },
+    'FINNIFTY': {
+        'instrument_token': 2601057,
+        'exchange': 'NSE',
+        'lot_size': 25,
+        'cpr_level': 18000,
+        'trend': 'uptrend',
+        'full_name': 'FINNIFTY'
+    }
+}
